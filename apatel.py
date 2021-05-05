d1ae6097496f0fecf3fdd154ccc1f19396bd8177
Avani Patel <apat382@gmail.com>
2021-05-05 12:42:21 -0500

    adding nutrient variables

--- apatel.py
+++ apatel.py
@@ -5,8 +5,6 @@ import requests, json, sys, time
 from pprint import pprint
 import pandas
 
-# import pycurl
-
 
 print(
     "please enter what food item you would like to cook, and we will provide the recipe"
@@ -24,36 +22,51 @@ recipeData = json.loads(response.text)
 r = recipeData["hits"]
 for item in r:
     # print(item["recipe"]["label"])
-    chosenfoodItem2 = str(item["recipe"]["label"])
+    # chosenfoodItem2 = str(item["recipe"]["label"])
     # recipeIngredients = item["recipe"]["ingredientLines"]
+    # print(chosenfoodItem2)
+
+    # print name
+    chosenfoodItem2 = str(item["recipe"]["label"])
     print(chosenfoodItem2)
+
+    # print calories
     chosenfoodCalories = str(item["recipe"]["calories"])
     print(chosenfoodCalories)
-    chosenNutrientsTotal = str(item["recipe"]["totalNutrients"])
-    pprint(chosenNutrientsTotal)
-    # nutrientLabel = str(item["nutrientinfo"]["label"])
-    # print(nutrientLabel)
+
+    # print total fat
+    totalFat = str(item["recipe"]["totalNutrients"]["FAT"]["quantity"])
+    print(totalFat)
+
+    # print cholesterol
+    totalCHOLE = str(item["recipe"]["totalNutrients"]["CHOLE"]["quantity"])
+    print(totalCHOLE)
+
+    # print sugars
+    totalSugar = str(item["recipe"]["totalNutrients"]["SUGAR"]["quantity"])
+    print(totalSugar)
+
+    # print protein
+    totalProtein = str(item["recipe"]["totalNutrients"]["PROCNT"]["quantity"])
+    print(totalProtein)
+
+
 # for ingredient in item["recipe"]["ingredientLines"]:
 #     print(ingredient)
 
 # foodItem(r)
 
-# print(
-#     "Which of the following recipes would you like to use? We will provide ingredients for that recipe."
-# )
-# chosenRecipe = input()
-# chosenfoodItem3 = chosenRecipe
+print(
+    "Which of the following recipes would you like to use? We will provide ingredients for that recipe."
+)
+chosenRecipe = input()
+chosenfoodItem3 = chosenRecipe
 
-# for ingredient in item["recipe"]["ingredientLines"]:
-#     print(ingredient)
+for ingredient in item["recipe"]["ingredientLines"]:
+    print(ingredient)
 
-# url = f"https://api.edamam.com/api/nutrition-details?app_id=2ac3e688&app_key=5f0e27adae50eb576e4aebe320bbefe2"
-# curl -d @chosenfoodItem3.json -H "Content-Type: application/json" "https://api.edamam.com/api/nutrition-details?app_id=2ac3e688&app_key=2ac8a94c3c3bd1be3dbed7d629136176"
-
-
-# response = requests.get(url)
-# response.raise_for_status()  # check for errors
-
-# jsonData = json.loads(response.text)
-# # r = recipeData["hits"]
-# pprint(jsonData)
+# Calories
+# Total fat
+# Cholesterol
+# Sugars
+# Protein
